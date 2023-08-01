ddddf8e0b52487646eacda3f1c714c27ced16455
zagger <huangzijie@os-easy.com>
2023-08-01 19:01:24 +0800

    fix: windows上配置git bash

---
+++ plugins/git.lua
@@ -0,0 +1,25 @@
+local get_icon = require("astronvim.utils").get_icon
+return {
+  "lewis6991/gitsigns.nvim",
+  enabled = vim.fn.executable "git" == 1,
+  event = "User AstroGitFile",
+  opts = {
+    signs = {
+      add = { text = get_icon "GitSign" },
+      change = { text = get_icon "GitSign" },
+      delete = { text = get_icon "GitSign" },
+      topdelete = { text = get_icon "GitSign" },
+      changedelete = { text = get_icon "GitSign" },
+      untracked = { text = get_icon "GitSign" },
+    },
+    worktrees = vim.g.git_worktrees,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 400,
+      ignore_whitespace = false,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  },
+}
--- polish.lua
+++ polish.lua
@@ -1,3 +1,7 @@
 -- This function is run last and is a good place to configuring
 -- anything that doesn't fit in the normal config can go here
-return function() require "user.autocmds" end
+return function() 
+  vim.cmd [[let &shell = '"D:/GlobalScoopApps/apps/git/current/bin/bash.exe"']]
+  vim.cmd [[let &shellcmdflag = '-s -l -i']]
+  require "user.autocmds" 
+end
